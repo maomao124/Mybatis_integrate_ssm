84c41dc486a1ce07369b16b445bd6c53eae177f3
毛 <1296193245@qq.com>
2022-03-17 21:57:35 +0800

    Mybatis_integrate_ssm

---
+++ src/main/java/mao/mybatis_integrate_ssm/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package mao.mybatis_integrate_ssm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Project name(项目名称)：Mybatis_integrate_ssm
+ * Package(包名): mao.mybatis_integrate_ssm
+ * Class(类名): AccountServiceImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/17
+ * Time(创建时间)： 21:18
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService
+{
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public void save(Account account)
+    {
+        accountMapper.save(account);
+    }
+
+    @Override
+    public List<Account> findAll()
+    {
+        return accountMapper.findAll();
+    }
+}
